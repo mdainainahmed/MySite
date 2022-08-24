1c95490c3939047e2e615c6aafca6b319ce36653
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2022-08-25 00:28:57 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// var i = 1;
-//
-// var id = setInterval(function() {
-//   document.querySelector("img").attributes[0].textContent = "images/img"+i+".jpg";
-//   i++;
-// },1000);
-//
-// if(i==4)
-//   clearInterval(id);
