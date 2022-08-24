651871257114317ac402f5ee351a7a5b466c5866
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2022-08-25 00:24:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// var i = 1;
+//
+// var id = setInterval(function() {
+//   document.querySelector("img").attributes[0].textContent = "images/img"+i+".jpg";
+//   i++;
+// },1000);
+//
+// if(i==4)
+//   clearInterval(id);
